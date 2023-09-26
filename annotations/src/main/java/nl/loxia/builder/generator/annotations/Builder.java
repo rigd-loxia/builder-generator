a281a828b7292a2958bf952cba51bb4fffa50494
Chris Notenboom <christian.notenboom@rigd-loxia.nl>
2023-09-26 11:24:36 +0200

    change <BR/> to <BR> as it caused problems building the project with java11

--- annotations/src/main/java/nl/loxia/builder/generator/annotations/Builder.java
+++ annotations/src/main/java/nl/loxia/builder/generator/annotations/Builder.java
@@ -7,8 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * Annotation used for marking classes for which a Builder needs to be generated. Placing this annotation on an `interface` or
- * `enum` is not supported. `Records` are untested.<BR/>
- * <BR/>
+ * `enum` is not supported. `Records` are untested.<BR>
+ * <BR>
  * The generated class will have the same name as the class on which the annotation is placed but suffixed with `Builder`.
  *
  * @author Ben Zegveld
