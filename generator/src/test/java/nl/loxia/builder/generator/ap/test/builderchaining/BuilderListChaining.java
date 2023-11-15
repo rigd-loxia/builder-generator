49f2263a91ebae398a579b7a3fba6d91b7e5f43e
Ben Zegveld <ben.zegveld@rigd-loxia.nl>
2023-11-15 13:19:52 +0100

    #18 restored support for initialized lists with only getter.

--- generator/src/test/java/nl/loxia/builder/generator/ap/test/builderchaining/BuilderListChaining.java
+++ generator/src/test/java/nl/loxia/builder/generator/ap/test/builderchaining/BuilderListChaining.java
@@ -7,13 +7,9 @@ import nl.loxia.builder.generator.annotations.Builder;
 
 @Builder
 public class BuilderListChaining {
-    private List<BuilderChainingChild> values = new ArrayList<>();
+    private final List<BuilderChainingChild> values = new ArrayList<>();
 
     public List<BuilderChainingChild> getValues() {
         return values;
-    }
-
-    public void setValues(List<BuilderChainingChild> values) {
-        this.values = values;
     }
 }
